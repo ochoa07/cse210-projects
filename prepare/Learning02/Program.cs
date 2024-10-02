b9a7d87d3491aa99a90ba49b46aa7a8aabb367a5
Chelsea Ochoa Mata <och23008@byui.edu>
2024-10-02 16:43:09 -0600

    new code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    }
+}
+
+// Job class definition
+class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+}
+
+// Resume class definition
+class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>(); // Initialize list to avoid null reference
+
+    public void Display()
+    {
+        Console.WriteLine($"Resume of {_name}");
+        foreach (var job in _jobs)
+        {
+            Console.WriteLine($"{job._jobTitle} at {job._company} from {job._startYear} to {job._endYear}");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        List<string> animals = new List<string>();
+        animals.Add("goat");
+        animals.Add("sheep");
+        animals.Add("cow");
+
+        Console.WriteLine("Animals: ") ;   
+        foreach (string animal in animals);
+            Console.WriteLine($" *{animals}");
     }
 }
